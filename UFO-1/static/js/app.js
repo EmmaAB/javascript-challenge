5f4a707342a4bbdab7b14371cde6edcc4d39dcb1
EmmaAB <66276192+EmmaAB@users.noreply.github.com>
2020-09-21 14:16:21 -0700

    Add files via upload

--- UFO-1/static/js/app.js
+++ UFO-1/static/js/app.js
@@ -1,46 +1,72 @@
 // from data.js
 var sightings = data;
 
-// // Select the button
- var button = d3.select("#button");
+//Reference the table body
+var tbody = d3.select("tbody");
 
-// // Select the form
- var form = d3.select("#datetime");
+data.forEach(function(sightings) {
+  console.log(sightings);
+  var row = tbody.append("tr");
+
+  Object.entries(sightings).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the UFO sightings object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+// // Select the button
+ var button = d3.select("#filter-btn");
+
+// // // Select the form
+var form = d3.select("form");
 
 // // Create event handlers 
-// button.on("click", runEnter);
-// form.on("submit",runEnter);
+button.on("click", runEnter);
+form.on("submit",runEnter);
 
-// // Complete the event handler function for the form
-// function runEnter() {
-
-//   // Prevent the page from refreshing
-//   d3.event.preventDefault();
+// Complete the event handler function for the form
+function runEnter() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
   
-//   // Select the input element and get the raw HTML node
-//   var inputElement = d3.select("#datetime.form-control");
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
 
-//   // Get the value property of the input element
-//   var inputValue = inputElement.property("value");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
 
-//   console.log(inputValue);
-//   console.log(sightings);
+  console.log(inputValue);
 
-//   var filteredData = people.filter(person => person.bloodType === inputValue);
+  //Define filteredData
+ var filteredData = sightings.map(sighting => sighting)
 
-//   console.log(filteredData);
+//  //Spread Operator
+//  var filteredData = [...sightings]
 
-// var tbody = d3.select("tbody");
-// console.log(data);
+if(inputValue){
+    filteredData = sightings.filter(sighting => sighting.datetime === inputValue);
+    console.log(filteredData);
+}
+ 
+tbody.html("")
 
-// data.forEach(function(sightings) {
-//   console.log(sightings);
-//   var row = tbody.append("tr");
-//   Object.entries(sightings).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the UFO sightings object
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
+filteredData.forEach(function(sighting) {
+  console.log(sighting);
+
+  //Add rows for filtered data sightings
+  var row = tbody.append("tr");
+  Object.entries(sighting).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the filtered UFO sightings object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+}
+
+
+
